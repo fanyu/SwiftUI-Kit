8e9f3fa66732b3a9c7d4a697e8cb507505a1a9a9
fanyu <fanyu@kuaishou.com>
2020-07-23 11:44:24 +0800

    init

---
+++ SwiftUI Kit/Shared/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by FanYu on 2020/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
